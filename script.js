a059a5df6b935987dfab5a9dd01d40bbfa76d36e
amelialein <163997050+amelialein@users.noreply.github.com>
2024-05-24 13:40:59 +0930

    add JavaScript to load different pages. version to submit!

--- script.js
+++ script.js
@@ -21,3 +21,80 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
 }
+
+
+
+
+// Load Fixture Page
+const loadFixture = () => {
+  document.querySelector('.home').style.display = 'none';
+  document.querySelector('.stats').style.display = 'none';
+  document.querySelector('.interviews').style.display = 'none';
+  document.querySelector('.join').style.display = 'none';
+  document.querySelector('.contact').style.display = 'none';
+  document.querySelector('.fixture').style.display = 'flex';
+  };
+
+document.querySelector('.navButtonFix').onclick = loadFixture;
+
+// Load Stats Page
+const loadStats = () => {
+  document.querySelector('.home').style.display = 'none';
+  document.querySelector('.fixture').style.display = 'none';
+  document.querySelector('.interviews').style.display = 'none';
+  document.querySelector('.join').style.display = 'none';
+  document.querySelector('.contact').style.display = 'none';
+  document.querySelector('.stats').style.display = 'flex';
+  };
+
+document.querySelector('.navButtonStats').onclick = loadStats;
+
+
+// Load Interviews Page
+const loadInterviews = () => {
+  document.querySelector('.home').style.display = 'none';
+  document.querySelector('.fixture').style.display = 'none';
+  document.querySelector('.stats').style.display = 'none';
+  document.querySelector('.join').style.display = 'none';
+  document.querySelector('.contact').style.display = 'none';
+  document.querySelector('.interviews').style.display = 'flex';
+  };
+
+document.querySelector('.navButtonInt').onclick = loadInterviews;
+
+// Load Join Page
+const loadJoin = () => {
+  document.querySelector('.home').style.display = 'none';
+  document.querySelector('.fixture').style.display = 'none';
+  document.querySelector('.stats').style.display = 'none';
+  document.querySelector('.interviews').style.display = 'none';
+  document.querySelector('.contact').style.display = 'none';
+  document.querySelector('.join').style.display = 'flex';
+  };
+
+document.querySelector('.navButtonJoin').onclick = loadJoin;
+
+// Load Contact Page
+const loadContact = () => {
+  document.querySelector('.home').style.display = 'none';
+  document.querySelector('.fixture').style.display = 'none';
+  document.querySelector('.stats').style.display = 'none';
+  document.querySelector('.join').style.display = 'none';
+  document.querySelector('.interviews').style.display = 'none';
+  document.querySelector('.contact').style.display = 'block';
+  };
+
+document.querySelector('.navButtonContact').onclick = loadContact;
+
+// Load Home Page
+const loadHome = () => {
+  document.querySelector('.interviews').style.display = 'none';
+  document.querySelector('.fixture').style.display = 'none';
+  document.querySelector('.stats').style.display = 'none';
+  document.querySelector('.join').style.display = 'none';
+  document.querySelector('.contact').style.display = 'none';
+  document.querySelector('.home').style.display = 'grid';
+  };
+
+document.querySelector('#logo').onclick = loadHome;
+
